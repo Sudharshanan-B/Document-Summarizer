b1bfcafe7e605bfc79b53a77902bf3ff1c5e0895
Sudharshanan Balaji <pbsudharshanan@gmail.com>
2023-11-07 14:15:05 +0530

    finished with components

---
+++ src/components/landing.jsx
@@ -0,0 +1,10 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1 className="uppercase pl-[40px] pb-[35px] text-[40px] font-bold text-headerColor">Worried a big document?</h1>
+      <h2 className="lowercase pl-[50px] text-[20px] font-medium text-headerColor">Don't worry we have the solution for summarizing it</h2>
+    </div>
+  );
+};
+
+export default Landing;
